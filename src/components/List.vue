50dcefb71871d5f55a6f39d21d498509fcee9525
Sergiyan92 <sergiyan@ukr.net>
2023-10-12 11:36:19 +0300

    create vue app

---
+++ src/components/List.vue
@@ -0,0 +1,17 @@
+<template>
+  <ul>
+    <li v-for="person in people">{{ person }}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      people: ["Petya", "Vasya", "Grisha", "Slava", "Senya"]
+    };
+  }
+};
+</script>
+
+<style></style>
