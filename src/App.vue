ae7a2597a09b9f7dfee2b9cba28837c446affdd1
Sergiyan92 <sergiyan@ukr.net>
2023-10-12 14:19:22 +0300

    Slot

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
   <div id="app">
+    <Slot>
+      <h2 slot="title">{{ slotTitle }}</h2>
+      <p slot="text">Text test for slot</p>
+    </Slot>
     <button type="button" @click="component = 'Reg'">Reg</button>
     <button type="button" @click="component = 'Aut'">Aut</button>
     <KeepAlive><component :is="component"></component></KeepAlive>
@@ -11,17 +15,20 @@ import List from "./components/List.vue";
 import Aut from "./components/Aut.vue";
 import Reg from "./components/Reg.vue";
 import Heder from "./components/Heder.vue";
+import Slot from "./components/Slot.vue";
 export default {
   components: {
     List,
     Aut,
     Reg,
-    Heder
+    Heder,
+    Slot
   },
   data() {
     return {
       title: "My app",
-      component: "Aut"
+      component: "Aut",
+      slotTitle: "Test title"
     };
   }
 };
