f514c8defc92bea535e72ff656f50deb62348f09
Sergiyan92 <sergiyan@ukr.net>
2023-10-12 14:27:24 +0300

    css

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,7 +1,10 @@
 <template>
-  <ul>
-    <li v-for="person in people">{{ person }}</li>
-  </ul>
+  <div>
+    <h2>Inner title</h2>
+    <ul>
+      <li v-for="person in people">{{ person }}</li>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -14,4 +17,8 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+h2 {
+  color: green;
+}
+</style>
